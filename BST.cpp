d33b6faea3e5185f6431fa724ddac9537fee7e60
Kang-Gumsil <rmatlf0708@naver.com>
2020-04-27 12:31:52 +0900

    update [deleteNode]

--- BST.cpp
+++ BST.cpp
@@ -58,91 +58,94 @@ bool BST::insertNode(int num)
 	return true;
 }
 
+
 bool BST::deleteNode(int num)
 {
 	// 삭제할 노드 p 찾고, 만약 없으면 false 리턴
 	Node* p = search(num);
 	bool returnValue = false;
-	if (!p)
-		return false;
 
-	// p의 부모노드 q 찾기, 만약 q의 자식 중 하나가 p면 찾은것
-	Node* q = root;
-	if (p != root)
-	{
-		while (1)
+	if (p) {
+		// p의 부모노드 q 찾기, 만약 q의 자식 중 하나가 p면 찾은것
+		Node* q = root;
+		if (p != root)
 		{
-			if (q->leftChild == p || q->rightChild == p)
-				break;
+			while (1)
+			{
+				if (q->leftChild == p || q->rightChild == p)
+					break;
 
-			if (num > q->data)
-				q = q->rightChild;
+				if (num > q->data)
+					q = q->rightChild;
+
+				else
+					q = q->leftChild;
+			}
+		}
+
+		// p의 자식 수
+		int count = 0;
+		if (p->leftChild) count++;
+		if (p->rightChild) count++;
+
+		if (count == 0) // 단말노드
+		{
+
+			// 삭제할 노드를 삭제
+			if (p == root)
+				root = 0;
+
+			else if (p == q->rightChild)
+				q->rightChild = 0;
 
 			else
-				q = q->leftChild;
+				q->leftChild = 0;
 		}
+
+		else if (count == 1) // 자식이 한개
+		{
+			// 삭제할 노드를 삭제하고, 그 자리를 자식노드로 채움
+			Node* child;
+
+			if (p->rightChild)
+				child = p->rightChild;
+
+			else
+				child = p->leftChild;
+
+			// 대체한 자식노드와 삭제할 노드의 부모노드와 연결
+			if (p == root)
+				root = child;
+
+			else if (p == q->rightChild)
+				q->rightChild = child;
+
+			else
+				q->leftChild = child;
+
+		}
+
+		else // 자식이 두개
+		{
+			Node* temp = q = p;
+
+			for (p = p->leftChild; p->rightChild; p = p->rightChild)
+				q = p;
+
+			if (q == temp)
+				q->leftChild = p->leftChild;
+
+			else
+				q->rightChild = p->leftChild;
+
+			temp->data = p->data; //데이터 대체
+		}
+
+		delete p; // 실제로 삭제할 노드가 삭제된 것이 아니라 대체될 노드가 삭제 
+		returnValue = true;
 	}
-
-	// p의 자식 수
-	int count = 0;
-	if (p->leftChild) count++;
-	if (p->rightChild) count++;
-
-	if (count == 0) // 단말노드
-	{
-
-		// 삭제할 노드를 삭제
-		if (p == root)
-			root = 0;
-
-		else if (p == q->rightChild)
-			q->rightChild = 0;
-
-		else
-			q->leftChild = 0;
-	}
-
-	else if (count == 1) // 자식이 한개
-	{
-		// 삭제할 노드를 삭제하고, 그 자리를 자식노드로 채움
-		Node* child;
-
-		if (p->rightChild)
-			child = p->rightChild;
-
-		else
-			child = p->leftChild;
-
-		// 대체한 자식노드와 삭제할 노드의 부모노드와 연결
-		if (p == root)
-			root = child;
-
-		else if (p == q->rightChild)
-			q->rightChild = child;
-
-		else
-			q->leftChild = child;
-
-	}
-
-	else // 자식이 두개
-	{
-		Node* temp = q = p;
-
-		for (p = p->leftChild; p->rightChild; p = p->rightChild)
-			q = p;
-
-		if (q == temp)
-			q->leftChild = p->leftChild;
-
-		else
-			q->rightChild = p->leftChild;
-
-		temp->data = p->data; //데이터 대체
-	}
-
-	delete p; // 실제로 삭제할 노드가 삭제된 것이 아니라 대체될 노드가 삭제 
-	return true;
+		
+	return returnValue;
 }
 
 void BST::nodeTraversal() // driver
